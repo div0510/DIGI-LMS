522330e1e71a2b6c4163a3591e807affb74e2e84
div0510 <divyansh963@gmail.com>
2022-10-26 11:38:59 +0530

    basic setup complete

---
+++ backend/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const db = 'digi_lms';
+
+const dbUrl = `mongodb+srv://div0510:1234div89@cluster0.vmtfv.mongodb.net/${db}?retryWrites=true&w=majority`
+
+mongoose.connect()
+.then((result) => {
+    console.log('Database Connected');
+}).catch((err) => {
+    console.log('Database not Connected', err);
+});
+
+module.exports = mongoose;
